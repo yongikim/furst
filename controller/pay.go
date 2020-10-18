2e8b34771903ecccdff3f3ac1173f74bed3205e1
yongikim <yongi.kim08@gmail.com>
2020-10-19 00:27:24 +0900

    Define response struct

--- controller/pay.go
+++ controller/pay.go
@@ -10,6 +10,27 @@ import (
 
 type PaysController struct{}
 
+type ByCategory struct {
+	Name   string `json:"name"`
+	Amount int    `json:"amount"`
+}
+
+type ByDay struct {
+	Date       int          `json:"date"`
+	Amount     int          `json:"amount"`
+	ByCategory []ByCategory `json:"by_category"`
+}
+
+type Ranking struct {
+	Content string `json:"content"`
+	Amount  int    `json:"amount"`
+}
+
+type Response struct {
+	ByDay   []ByDay   `json:"by_day"`
+	Ranking []Ranking `json:"ranking"`
+}
+
 func (PaysController) GetPaysByYM(c *gin.Context) {
 	year, _ := strconv.Atoi(c.Query("year"))
 	month, _ := strconv.Atoi(c.Query("month"))
